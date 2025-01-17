cca948f5e5e66868f524fed2d8b0f0bd75d47992
BowenNB <2923591580@qq.com>
2025-01-17 17:27:06 +0800

    Initial commit

---
+++ geekbang/ALGO/u31/Client.java
@@ -0,0 +1,16 @@
+package geekbang.ALGO.u31;
+
+import edu.princeton.cs.algs4.In;
+
+/**
+ * Created by HuGuodong on 12/14/19.
+ */
+public class Client {
+
+  public static void main(String[] args) {
+    // build graph from file
+    Graph g = new Graph(new In("geekbang/ALGO/u31/data.txt"));
+    g.bfs(0, 6);
+//    0 1 4 6
+  }
+}
